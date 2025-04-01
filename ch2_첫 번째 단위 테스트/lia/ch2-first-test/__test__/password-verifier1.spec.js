50acced5a2b76b5714fee052eb13b9b376fac028
osiyeon <osiyun0313@gmail.com>
2025-04-02 00:00:42 +0900

    lia: ch2 마무리

--- ch2_첫 번째 단위 테스트/lia/ch2-first-test/__test__/password-verifier1.spec.js
+++ ch2_첫 번째 단위 테스트/lia/ch2-first-test/__test__/password-verifier1.spec.js
@@ -50,3 +50,35 @@ describe("PasswordVerifier", () => {
 		});
 	});
 });
+
+// 2-13. beforeEach() 함수를 2뎁스로 사용하기
+describe("PasswordVerifier", () => {
+	let verifier;
+
+	// verifier 변수를 두어 하위의 모든 테스트에서 사용할 수 있도록 설정
+	beforeEach(() => {
+		verifier = new PasswordVerifier1();
+	});
+
+	describe("with a failing rule", () => {
+		let fakeRule, errors;
+
+		// describe('with a failing rule') 구역 내에서 사용 가능한 fakeRule() 함수 설정
+		beforeEach(() => {
+			fakeRule = () => ({ passed: false, reason: "fake reason" });
+			verifier.addRule(fakeRule);
+		});
+
+		it("has an error message based on the rule.reason", () => {
+			errors = verifier.verify("any value");
+
+			expect(errors[0]).toContain("fake reason");
+		});
+
+		it("has exactly one error", () => {
+			errors = verifier.verify("any value");
+
+			expect(errors.length).toBe(1);
+		});
+	});
+});
